6fb9ab3d3422b35a5a1602ef03b34c008f91a40b
vinaysagar15 <48981515+vinaysagar15@users.noreply.github.com>
2021-02-04 15:09:56 +0530

    Create variables.tf

---
+++ tests/variables.tf
@@ -0,0 +1,90 @@
+variable "name" {
+  description = "Name of storage account, if it contains illegal characters (,-_ etc) those will be truncated."
+}
+
+variable "resource_group_name" {
+  description = "Name of resource group to deploy resources in."
+}
+
+variable "location" {
+  description = "Azure location where resources should be deployed."
+}
+
+variable "account_tier" {
+  description = "Defines the Tier to use for this storage account. Valid options are Standard and Premium. Changing this forces a new resource to be created."
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "Defines the type of replication to use for this storage account. Valid options are LRS, GRS, RAGRS and ZRS."
+  default     = "ZRS"
+}
+
+variable "access_tier" {
+  description = "Defines the access tier for BlobStorage and StorageV2 accounts. Valid options are Hot and Cool."
+  default     = "Hot"
+}
+
+variable "soft_delete_retention" {
+  description = "Number of retention days for soft delete. If set to null it will disable soft delete all together."
+  type        = number
+  default     = 31
+}
+
+variable "cors_rule" {
+  description = "CORS rules for storage account."
+  type = list(object({
+    allowed_origins    = list(string),
+    allowed_methods    = list(string),
+    allowed_headers    = list(string),
+    exposed_headers    = list(string),
+    max_age_in_seconds = number
+  }))
+  default = []
+}
+
+variable "enable_advanced_threat_protection" {
+  description = "Boolean flag which controls if advanced threat protection is enabled."
+  type        = bool
+  default     = false
+}
+
+variable "network_rules" {
+  description = "Network rules restricting access to the storage account."
+  type = object({
+    ip_rules   = list(string),
+    subnet_ids = list(string),
+    bypass     = list(string)
+  })
+  default = null
+}
+
+variable "containers" {
+  description = "List of containers to create and their access levels."
+  type = list(object({
+    name        = string,
+    access_type = string
+  }))
+  default = []
+}
+
+variable "events" {
+  description = "List of event subscriptions. See documentation for format description."
+  type        = list(any)
+  default     = []
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources created."
+  type        = map(string)
+  default     = {}
+}
+
+variable "lifecycles" {
+  description = "List of lifecycle delete"
+  type = list(object({
+    prefix_match      = set(string),
+    delete_after_days = number
+  }))
+  default = []
+}
