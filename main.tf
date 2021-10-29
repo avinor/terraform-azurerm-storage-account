132374380f4cb129507eb163aff96f32123f672a
Øyvind Strømmen <insula.ventus@gmail.com>
2021-10-29 13:09:31 +0200

    Use azurerm v2.83.0

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.56.0"
+      version = "~> 2.83.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -12,7 +12,7 @@ terraform {
   }
 }
 
-provider azurerm {
+provider "azurerm" {
   features {}
 }
 
